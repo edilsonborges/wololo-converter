567575912677bcbaba595304800049ca520b25d4
Edilson Borges <edilson.borges@goias.gov.br>
2026-03-13 00:48:10 -0300

    feat: US-007 - Frontend - componente de preview com thumbnail

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -4,3 +4,4 @@ export { ProgressCard } from './ProgressCard';
 export { MultiURLInput, type ParsedURL } from './MultiURLInput';
 export { QueueManager } from './QueueManager';
 export { Icon, PlatformIcon, FormatIcon, type IconName } from './Icon';
+export { VideoPreview } from './VideoPreview';
